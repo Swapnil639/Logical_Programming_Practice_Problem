45eb15176b5c6d141a7e5619de2b9480078736ad
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-08-17 18:33:30 +0530

    Added :Find Prime Number or not

---
+++ PrimeNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int temp=0;
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                temp=temp+1;
+            }
+        }
+        if(temp>0)
+            System.out.println(n+" is not prime number");
+        else
+            System.out.println(n+" is prime number");
+    }
+
+}
